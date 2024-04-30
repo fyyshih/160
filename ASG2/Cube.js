e41fad8b8f858440c04764ef27179dd89defb2d4
fyyshih <felithecity47@gmail.com>
2024-04-30 03:58:09 -0700

    2.1

---
+++ ASG2/Cube.js
@@ -0,0 +1,39 @@
+class Cube {
+    constructor() {
+        this.type = "cube";
+        this.color = [1.0, 1.0, 1.0, 1.0];
+        this.matrix = new Matrix4();
+    }
+
+    render() {
+        var rgba = this.color;
+
+        // pass color of point to u_FragColor
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        
+        // front face
+        drawTriangle3D([0.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,0.0]);
+        drawTriangle3D([0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0]);
+
+        // top face
+        drawTriangle3D([0.0,1.0,0.0, 1.0,1.0,-1.0, 1.0,1.0,1.0]);
+        drawTriangle3D([0.0,1.0,0.0, 0.0,1.0,-1.0, 1.0,1.0,-1.0]);
+
+        // bottom face
+        drawTriangle3D([0.0,0.0,0.0, 1.0,0.0,-1.0, 1.0,0.0,1.0]);
+        drawTriangle3D([0.0,0.0,0.0, 0.0,0.0,-1.0, 1.0,0.0,-1.0]);
+
+        // back face
+        drawTriangle3D([0.0,0.0,-1.0, 1.0,1.0,-1.0, 1.0,0.0,-1.0]);
+        drawTriangle3D([0.0,0.0,-1.0, 0.0,1.0,-1.0, 1.0,1.0,-1.0]);
+
+        // left face
+        drawTriangle3D([0.0,0.0,0.0, 0.0,1.0,0.0, 0.0,0.0,-1.0]);
+        drawTriangle3D([0.0,0.0,-1.0, 0.0,1.0,-1.0, 0.0,1.0,0.0]);
+
+        // right face
+        drawTriangle3D([1.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,-1.0]);
+        drawTriangle3D([1.0,1.0,0.0, 1.0,1.0,-1.0, 1.0,0.0,-1.0]);
+
+    }
+}
---
+++ ASG2/Triangle.js
@@ -0,0 +1,64 @@
+class Triangle {
+    constructor() {
+      this.type="triangle";
+      this.position = [[0.0, 0.0],  [0.0, 0.0], [0.0, 0.0]];
+      this.color = [1.0, 1.0, 1.0, 1.0];
+      this.size = 5.0;
+    }
+  
+    render() {
+      var xy = this.position;
+      var rgba = this.color;
+      var size = this.size;
+          
+      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], 1.0);
+      gl.uniform1f(u_Size, size);
+  
+      var d = this.size/200.0;
+      drawTriangle([xy[0], xy[1], xy[0]+d, xy[1], xy[0], xy[1]+d]);
+    }
+  }
+  
+  
+  // HelloTriangle.js (c) 2012 matsuda
+  function drawTriangle(vertices) {
+    var n = 3; // The number of vertices
+  
+    // buffer
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+      console.log('Failed to create the buffer object');
+      return -1;
+    }
+    // bind buffer obj to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // write data into buffer obj
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+    // enable assignment to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+  
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+  }
+
+  function drawTriangle3D(vertices) {
+    var n = 3; // The number of vertices
+  
+    // buffer
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+      console.log('Failed to create the buffer object');
+      return -1;
+    }
+    // bind buffer obj to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // write data into buffer obj
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+    // enable assignment to a_Position variable
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0); // change second param to 3 for 3D
+    gl.enableVertexAttribArray(a_Position);
+  
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+  }
