90a6a2611da221d10bd9922ce1fd29f3f2c3808b
fyyshih <felithecity47@gmail.com>
2024-05-01 11:22:34 -0700

    legs -> hexahedron

--- ASG2/BlockyAnimal.js
+++ ASG2/BlockyAnimal.js
@@ -85,21 +85,25 @@ const CIRCLE = 2;
 // UI global elements
 let g_baseNeckAnimation = false;
 let g_upperNeckAnimation = false;
+let g_headAnimation = false;
 let g_globalAngle = 0;
 let g_baseNeckAngle = 0;
 let g_upperNeckAngle = 0;
+let g_headAngle = 45;
 
 function addActionsForHtmlUI() {
-    // button events: animation on/off (both neck joints))
+    // button events: animation on/off (both neck joints + head)
     document.getElementById('animationBaseNeckOnButton').onclick = function() {g_baseNeckAnimation = true;};
     document.getElementById('animationBaseNeckOffButton').onclick = function() {g_baseNeckAnimation = false;};
     document.getElementById('animationUpperNeckOnButton').onclick = function() {g_upperNeckAnimation = true;};
     document.getElementById('animationUpperNeckOffButton').onclick = function() {g_upperNeckAnimation = false;};
+    document.getElementById('animationHeadOnButton').onclick = function() {g_headAnimation = true;};
+    document.getElementById('animationHeadOffButton').onclick = function() {g_headAnimation = false;};
 
-    // slider events: neck angle (both joints)
+    // slider events: head + neck angle (both joints) 
     document.getElementById('baseNeckSlide').addEventListener('mousemove', function() { g_baseNeckAngle = this.value; renderScene(); });
     document.getElementById('upperNeckSlide').addEventListener('mousemove', function() { g_upperNeckAngle = this.value; renderScene(); });
- 
+    document.getElementById('headSlide').addEventListener('mousemove', function() {g_headAngle = this.value; renderScene(); });
     // slider event: camera angle
     document.getElementById("angleSlide").addEventListener("mousemove", function() { g_globalAngle = this.value; renderScene(); } );    
 }
@@ -145,6 +149,9 @@ function updateAnimationAngles() {
     if (g_upperNeckAnimation) {
         g_upperNeckAngle = 45*Math.sin(g_seconds);
     }
+    if (g_headAnimation) {
+        g_headAngle = 30*Math.sin(g_seconds);
+    }
 }
 
 function renderScene() {
@@ -183,16 +190,16 @@ function renderScene() {
     rightWing.matrix.scale(.4, .7, .2);
     rightWing.render();
 
-    // draw left leg (top joint)
-    var leftLeg = new Cube();
+    // draw left leg 
+    var leftLeg = new Hexahedron();
     leftLeg.color = [1, 0, 1, 1];
     leftLeg.matrix.translate(.4, -.8, -.25);
     leftLeg.matrix.rotate(90, 1, 0, 0);
     leftLeg.matrix.scale(-.1, -.1, .3);
     leftLeg.render();
 
-    // draw right leg (top joint)
-    var rightLeg = new Cube();
+    // draw right leg 
+    var rightLeg = new Hexahedron();
     rightLeg.color = [1, 0.5, 0.5, 1];
     rightLeg.matrix.translate(.4, -.8, -.1);
     rightLeg.matrix.rotate(90, 1, 0, 0);
@@ -232,32 +239,40 @@ function renderScene() {
     upperNeck.matrix.translate(0, .35, 0);
     upperNeck.matrix.rotate(315, 0, 0, 1);
     upperNeck.matrix.rotate(g_upperNeckAngle, 0, 0, 1);
+    var upperNeckCoordinatesMat = new Matrix4(upperNeck.matrix);
     upperNeck.matrix.scale(.1, .5, .12);
-    upperNeck.matrix.translate(-.3, .05, -0.001);
+    upperNeck.matrix.translate(-.1, .05, -0.001);
     upperNeck.render();
 
     // draw head
     var head = new Cube();
     head.color = [0.5, 0.8, 0.5, 1];
-    head.matrix.translate(-.28, .43, -.12);
-    head.matrix.rotate(30, 0, 0, 1);
+    head.matrix = upperNeckCoordinatesMat;
+    head.matrix.scale(-1, 1, 1); // reflect across y axis
+    head.matrix.translate(-.08, .4, .03);
+    head.matrix.rotate(g_headAngle, 0, 1, -1);
+    head.matrix.rotate(30, 0, -1, 1);
+    var headCoordinatesMat = new Matrix4(head.matrix);
     head.matrix.scale(.25, .2, .2);
     head.render();
 
     // draw upper beak
     var upperBeak = new Cube();
     upperBeak.color = [0.5, 0.2, 0.5, 1];
-    upperBeak.matrix.translate(-.73, .25, -.17);
-    upperBeak.matrix.rotate(30, 0, 0, 1);
+    upperBeak.matrix = headCoordinatesMat;
+    upperBeak.matrix.scale(1, -1, 1);
+    upperBeak.matrix.translate(.2, -.1, -.05);
+    upperBeak.matrix.rotate(0, 0, 0, 1);
     upperBeak.matrix.scale(.5, .05, .1);
     upperBeak.render();
 
     // draw lower beak
     var lowerBeak = new Cube();
     lowerBeak.color = [0.5, 0.5, 0.8, 1];
-    lowerBeak.matrix.translate(-.7, .2, -.17);
-    lowerBeak.matrix.rotate(30, 0, 0, 1);
-    lowerBeak.matrix.scale(.5, .05, .1);
+    lowerBeak.matrix = headCoordinatesMat;
+    lowerBeak.matrix.scale(1, -1, 1);
+    lowerBeak.matrix.translate(0, -1.9, 0);
+    lowerBeak.matrix.rotate(0, 0, 0, 1);
     lowerBeak.render();
 
     var duration = performance.now() - startTime;
