76b4854d2dc7182b6a0435cceb19fddf8d204291
fyyshih <felithecity47@gmail.com>
2024-05-17 15:28:28 -0700

    after 3.6a

--- ASG3/world.js
+++ ASG3/world.js
@@ -322,16 +322,22 @@ function updateAnimationAngles() {
     }
 }
 
+// set up variables to control the camera
+var g_eye = [0,0,3];
+var g_at = [0,0,-100];
+var g_up = [0,1,0];
 function renderScene() {
 
     var startTime = performance.now();
 
     // pass the projection matrix
     var projMat = new Matrix4();
+    projMat.setPerspective(50, canvas.width/canvas.height, 1, 100);
     gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
 
     // pass the view matrix
     var viewMat = new Matrix4();
+    viewMat.setLookAt(g_eye[0],g_eye[1],g_eye[2], g_at[0],g_at[1],g_at[2], g_up[0],g_up[1],g_up[2]); // (eye, at, up)
     gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements);
 
     // pass the matrix to u_ModelMatrix attribute
